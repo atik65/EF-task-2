7c63d92cc237e20c3170b2392b69661dd1afedc8
atik65 <atikhasan801@gmail.com>
2023-08-24 18:21:58 +0600

    fix: responsive layout strutuce fix for all device

--- src/js/main.js
+++ src/js/main.js
@@ -15,12 +15,21 @@ container.addEventListener("wheel", (event) => {
 const adjustWidth = () => {
   const screenWidth = window.innerWidth;
   let containerWidth;
-  if (screenWidth > 992) {
-    containerWidth = ((screenWidth - 220) / 10) * 7;
-  } else if (screenWidth > 576) {
-    containerWidth = (screenWidth / 10) * 7;
-  } else {
+
+  if (screenWidth > 0 && screenWidth < 576) {
     containerWidth = screenWidth - 30;
+  } else if (screenWidth > 576 && screenWidth < 768) {
+    containerWidth = screenWidth - 50;
+  } else if (screenWidth > 768 && screenWidth < 992) {
+    containerWidth = (screenWidth / 10) * 6 - 20;
+  } else if (screenWidth > 992 && screenWidth < 1100) {
+    containerWidth = (screenWidth / 10) * 4.5;
+  } else if (screenWidth > 1100 && screenWidth < 1200) {
+    containerWidth = (screenWidth / 10) * 5 - 50;
+  } else if (screenWidth > 1200 && screenWidth < 1440) {
+    containerWidth = (screenWidth / 10) * 5;
+  } else {
+    containerWidth = ((screenWidth - 220) / 10) * 7;
   }
   container.style.width = `${parseInt(containerWidth)}px`;
 };
